f84f85231ef1d5f3c285af1001f58a857c52abc3
Harsh Budhauliya <taliyotechnologies@gmail.com>
2026-03-26 21:20:36 +0530

    chore: cleanup dummy scripts

--- query.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config({path: '.env.local'});
-const { createClient } = require('@supabase/supabase-js');
-const sup = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
-sup.from('purchases').select('*').limit(3).then((res)=>console.log(res));
